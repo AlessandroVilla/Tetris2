018a8b53dd2e0c79565b8a17823a88b57ed2ce44
MonsterMunchStrabiste <alexandre.rastel@gmail.com>
2016-03-10 11:44:10 +0100

    debut des tests

---
+++ src/test/java/fr/esiea/cours/tetris/LauncherTest.java
@@ -0,0 +1,15 @@
+package fr.esiea.cours.tetris;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LauncherTest {
+
+	@Test
+	public void testMain() {
+		
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/fr/esiea/cours/tetris/ServeurTest.java
@@ -0,0 +1,16 @@
+package fr.esiea.cours.tetris;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ServeurTest {
+
+	@Test
+	public void testMain() {
+		Serveur serveur = new Serveur();
+		
+		fail("Not yet implemented");
+	}
+
+}
